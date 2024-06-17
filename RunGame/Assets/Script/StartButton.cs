f106f2312a4f6eacf296af4e2f2fe2ca318c4439
tejimatsuyoshi <tejima.tuyoshi@icloud.com>
2024-06-18 07:55:57 +0900

    初めと終わりの作成

---
+++ RunGame/Assets/Script/StartButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Button()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
